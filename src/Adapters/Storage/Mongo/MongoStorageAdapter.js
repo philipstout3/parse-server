0a8666b48c75877aab8dcd5cedded8a033b7417a
Philip Stout <philipstout@Philips-MacBook-Air.local>
2024-02-14 15:00:57 -0800

    dont throw error for code 86 index creation

--- src/Adapters/Storage/Mongo/MongoStorageAdapter.js
+++ src/Adapters/Storage/Mongo/MongoStorageAdapter.js
@@ -713,6 +713,10 @@ export class MongoStorageAdapter implements StorageAdapter {
             'Tried to ensure field uniqueness for a class that already has duplicates.'
           );
         }
+        if(error.code === 86) {
+            console.log(`Parse server wants us to drop the index for ${className}.${fieldNames} so they can recreate it. Not sure if we should.`);
+            return Promise.resolve();
+        }
         throw error;
       })
       .catch(err => this.handleError(err));
