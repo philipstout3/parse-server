2ddd585beaf1b0ddcb3893e6119888013ca2697a
Philip Stout <philipstout@Philips-MacBook-Air.local>
2024-02-19 11:31:25 -0800

    success, error defaults in response

--- src/Routers/FunctionsRouter.js
+++ src/Routers/FunctionsRouter.js
@@ -183,13 +183,14 @@ export class FunctionsRouter extends PromiseRouter {
         })
         .then(() => {
           //re-added the response to cloud functions
-          return new Promise((resolve, reject) => {
-            var _response = {
-              success: resolve,
-              error: reject,
-            };
-            return theFunction(request, _response);
-          });
+          return theFunction(request, { success, error });
+          //   return new Promise((resolve, reject) => {
+          //     var _response = {
+          //       success: resolve,
+          //       error: reject
+          //     };
+          //     return theFunction(request, _response);
+          //   });
         })
         .then(success, error);
     });
